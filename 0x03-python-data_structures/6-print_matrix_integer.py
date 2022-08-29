86f22819966ba2ad577b6f2e15863decfd48ceac
noahmuler <noahtaddese@gmail.com>
2022-08-28 23:30:40 -0700

    Hot Fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print(' '.join('{:d}'.format(j)for j in i))#!/usr/bin/python3
+        print(' '.join('{:d}'.format(j)for j in i))
